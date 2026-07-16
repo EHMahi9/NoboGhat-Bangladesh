c24bf9a89eba16f257c4bb95693aefd1f7c4cf66
EHMahi9 <vaibongo20@gmail.com>
2026-07-16 19:03:54 +0600

    Hero section added.

--- Noboghat/script.js
+++ Noboghat/script.js
@@ -1,14 +1,10 @@
-console.log("Hello! I am Mahi.")
+console.log("Hello! I am Mahi.");
 
-console.log("NoboGhat Portal Initialized - Developer: Mahi");
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
 
-// Wait for the HTML document to fully load
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    // Toggle the mobile menu when the hamburger icon is clicked
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
     });
 });
